25caa53e172daef605858122ea0fdd25a5165c26
fengzhuojia <179309463@qq.com>
2020-05-25 18:49:47 +0800

    eslint for button.js

--- src/core/components/button/button.js
+++ src/core/components/button/button.js
@@ -9,25 +9,24 @@ const Button = {
       const elem = $el[0];
       elem.disabled = true;
       elem.dataset.resetText = elem.innerText;
-      let text = elem.dataset.loadingText ? elem.dataset.loadingText : elem.innerText;
-      let color = elem.dataset.loadingIconClass ? elem.dataset.loadingIconClass : 'color-white';
-      let icon = "<span class='preloader "+color+"'></span>";
-      elem.innerHTML = elem.dataset.loadingIconPosition=="right" ?  (text + icon) : (icon + text);
+      const text = elem.dataset.loadingText ? elem.dataset.loadingText : elem.innerText;
+      const color = elem.dataset.loadingIconClass ? elem.dataset.loadingIconClass : 'color-white';
+      const icon = `<span class="preloader ${color}"></span>`;
+      elem.innerHTML = elem.dataset.loadingIconPosition === 'right' ? (text + icon) : (icon + text);
       $el.find('.preloader').each((index, preloaderEl) => {
         app.preloader.init(preloaderEl);
       });
     }
   },
   reset(el) {
-    const app = this;
     const $el = $(el);
     if ($el.length) {
       const elem = $el[0];
       elem.disabled = false;
       elem.innerHTML = elem.dataset.resetText;
     }
-  }
-}
+  },
+};
 
 export default {
   name: 'button',
@@ -39,5 +38,5 @@ export default {
         reset: Button.reset.bind(app),
       },
     });
-  },  
+  },
 };
