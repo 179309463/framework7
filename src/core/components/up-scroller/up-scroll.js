11ef1b01064a4bc52968da5a0257cbf435c85d73
fengzhuojia <179309463@qq.com>
2020-05-28 14:27:30 +0800

    up-scroll.js import Dom7

--- src/core/components/up-scroller/up-scroll.js
+++ src/core/components/up-scroller/up-scroll.js
@@ -1,3 +1,5 @@
+import $ from 'dom7';
+
 module.exports = {
 	name: 'upscroller',
 	params: {
@@ -14,48 +16,47 @@ module.exports = {
 
 			var app = this;
 			var params = app.params.upscroller;
-			var $$ = Dom7;
 
 			if (params.includeUpPages.includes(page.name)){
-                var $$up_btn = $$('<div class="upscroller">↑ ' + params.up_text + '</div>');
-                $$(page.el).prepend($$up_btn);
+                var $up_btn = $('<div class="upscroller">↑ ' + params.up_text + '</div>');
+                $(page.el).prepend($up_btn);
     
-                $$up_btn.click(function (event) {
+                $up_btn.click(function (event) {
                     event.stopPropagation();
                     event.preventDefault();
-                    var pageContent = $$('.page-content', page.el);
+                    var pageContent = $('.page-content', page.el);
                     pageContent.scrollTop(0, Math.round(pageContent.scrollTop() / 4));
                 });
     
-                $$(".page-content", page.el).scroll(function (event) {
-                    var e = $$(event.target).scrollTop();
+                $(".page-content", page.el).scroll(function (event) {
+                    var e = $(event.target).scrollTop();
                     if (e > 300) {
-                        $$up_btn.addClass('show');
+                        $up_btn.addClass('show');
                     } else {
-                        $$up_btn.removeClass('show');
+                        $up_btn.removeClass('show');
                     }
                 });
             }
 
             if (params.includeUpPages.includes(page.name)){
-                var $$down_btn = $$('<div class="downscroller">↓ ' + params.down_text + '</div>');
-                $$(page.el).append($$down_btn);
+                var $down_btn = $('<div class="downscroller">↓ ' + params.down_text + '</div>');
+                $(page.el).append($down_btn);
                 
-                $$down_btn.click(function (event) {
+                $down_btn.click(function (event) {
                     event.stopPropagation();
                     event.preventDefault();
-                    var pageContent = $$('.page-content', page.el);
+                    var pageContent = $('.page-content', page.el);
                     pageContent.scrollTop(pageContent[0].scrollHeight, Math.round((pageContent[0].scrollHeight-pageContent.scrollTop()) / 4));
                 });
     
-                $$(".page-content", page.el).scroll(function (event) {
-                    var e = $$(event.target).scrollTop();
-                    var contentHeight = $$(event.target)[0].scrollHeight;
-                    var winHeight = $$(window).height();
+                $(".page-content", page.el).scroll(function (event) {
+                    var e = $(event.target).scrollTop();
+                    var contentHeight = $(event.target)[0].scrollHeight;
+                    var winHeight = $(window).height();
                     if (contentHeight - winHeight - e < 300) {
-                        $$down_btn.addClass('hide');
+                        $down_btn.addClass('hide');
                     } else {
-                        $$down_btn.removeClass('hide');
+                        $down_btn.removeClass('hide');
                     }
                 });
             }
