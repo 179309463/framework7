b0756856452c545e62c885ad82344786c084a2f5
fengzhuojia <179309463@qq.com>
2020-05-28 18:03:13 +0800

    eslint for up-scroll.js

--- src/core/components/up-scroller/up-scroll.js
+++ src/core/components/up-scroller/up-scroll.js
@@ -1,67 +1,63 @@
 import $ from 'dom7';
 
 export default {
-	name: 'upscroller',
-	params: {
-		upscroller: {
-            up_text: '滚动至顶',
-            down_text: '滚动至底',
-            includeUpPages: [],
-            includeUpPages: [],
-		}
-	},
-	on: {
-		pageInit: function (page) {
-			'use strict';
+  name: 'upscroller',
+  params: {
+    upscroller: {
+      up_text: '滚动至顶',
+      down_text: '滚动至底',
+      includeUpPages: [],
+      includeDownPages: [],
+    },
+  },
+  on: {
+    pageInit(page) {
+      const app = this;
+      const params = app.params.upscroller;
 
-			var app = this;
-			var params = app.params.upscroller;
+      if (params.includeUpPages.includes(page.name)) {
+        const upBtn = $(`<div class="upscroller">↑ ${params.up_text}</div>`);
+        $(page.el).prepend(upBtn);
 
-			if (params.includeUpPages.includes(page.name)){
-                var $up_btn = $('<div class="upscroller">↑ ' + params.up_text + '</div>');
-                $(page.el).prepend($up_btn);
-    
-                $up_btn.click(function (event) {
-                    event.stopPropagation();
-                    event.preventDefault();
-                    var pageContent = $('.page-content', page.el);
-                    pageContent.scrollTop(0, Math.round(pageContent.scrollTop() / 4));
-                });
-    
-                $(".page-content", page.el).scroll(function (event) {
-                    var e = $(event.target).scrollTop();
-                    if (e > 300) {
-                        $up_btn.addClass('show');
-                    } else {
-                        $up_btn.removeClass('show');
-                    }
-                });
-            }
+        upBtn.click((event) => {
+          event.stopPropagation();
+          event.preventDefault();
+          const pageContent = $('.page-content', page.el);
+          pageContent.scrollTop(0, Math.round(pageContent.scrollTop() / 4));
+        });
 
-            if (params.includeUpPages.includes(page.name)){
-                var $down_btn = $('<div class="downscroller">↓ ' + params.down_text + '</div>');
-                $(page.el).append($down_btn);
-                
-                $down_btn.click(function (event) {
-                    event.stopPropagation();
-                    event.preventDefault();
-                    var pageContent = $('.page-content', page.el);
-                    pageContent.scrollTop(pageContent[0].scrollHeight, Math.round((pageContent[0].scrollHeight-pageContent.scrollTop()) / 4));
-                });
-    
-                $(".page-content", page.el).scroll(function (event) {
-                    var e = $(event.target).scrollTop();
-                    var contentHeight = $(event.target)[0].scrollHeight;
-                    var winHeight = $(window).height();
-                    if (contentHeight - winHeight - e < 300) {
-                        $down_btn.addClass('hide');
-                    } else {
-                        $down_btn.removeClass('hide');
-                    }
-                });
-            }
+        $('.page-content', page.el).scroll((event) => {
+          const e = $(event.target).scrollTop();
+          if (e > 300) {
+            upBtn.addClass('show');
+          } else {
+            upBtn.removeClass('show');
+          }
+        });
+      }
 
-			
-		},
-	}
+      if (params.includeDownPages.includes(page.name)) {
+        const downBtn = $(`<div class="downscroller">↓ ${params.down_text}</div>`);
+        $(page.el).append(downBtn);
+
+        downBtn.click((event) => {
+          event.stopPropagation();
+          event.preventDefault();
+          const pageContent = $('.page-content', page.el);
+          pageContent.scrollTop(pageContent[0].scrollHeight, Math.round((pageContent[0].scrollHeight - pageContent.scrollTop()) / 4));
+        });
+
+        $('.page-content', page.el).scroll((event) => {
+          const e = $(event.target).scrollTop();
+          const contentHeight = $(event.target)[0].scrollHeight;
+          const winHeight = $(window).height();
+          if (contentHeight - winHeight - e < 300) {
+            downBtn.addClass('hide');
+          } else {
+            downBtn.removeClass('hide');
+          }
+        });
+      }
+    },
+  },
 };
